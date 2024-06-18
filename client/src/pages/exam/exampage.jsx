84e3a42dff8f7afed3319211fd2750812f36f467
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-06-19 00:50:02 +0530

    Added Timer To the exam page

--- client/src/pages/exam/exampage.jsx
+++ client/src/pages/exam/exampage.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './exampage.css'
 import QuestionView from './questionview/questionview'
+import { useNavigate } from 'react-router-dom';
 
 function ExamPage() {
+    const navigate = useNavigate();
 
     const samplearray = [1,2,3,4,5,6,7,9,10];
+
+    function handleExamSubmit() {
+        navigate('/login');
+    }
+
+
+    const initialTime = 180 * 60;
+    const [seconds, setSeconds] = useState(20);
+
+    // Function to decrement the timer value
+    const tick = () => {
+        setSeconds(prevSeconds => (prevSeconds > 0 ? prevSeconds - 1 : handleExamSubmit()));
+    };
+
+    // Effect hook to run the tick function every second
+    useEffect(() => {
+        const timer = setInterval(() => tick(), 1000);
+        return () => clearInterval(timer); // Cleanup function to clear interval on unmount
+    }, []);
+
+    // Format seconds into hh:mm:ss format
+    const formatTime = (totalSeconds) => {
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+        return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    };
+
+    
+
 
     return (
         <div className='exampage'>
@@ -20,7 +52,7 @@ function ExamPage() {
                     </div>
                     <div className="sectionandtime bordersimple">
                         <label className='titlesection'>Section</label>
-                        <label className='timeleft'>Time Left : 00:00</label>
+                        <label className='timeleft'>Time Left : {formatTime(seconds)}</label>
                     </div>
                     <div className="sections bordersimple">
                         <div className={"section"}>Data Structures  </div>
@@ -81,7 +113,7 @@ function ExamPage() {
                         </div>
                     </div>
                     <div className="submitdiv">
-                        <button className="examsubmitbtn">Submit</button>
+                        <button className="examsubmitbtn" onClick={handleExamSubmit}>Submit</button>
                     </div>
                 </div>
             </div>
